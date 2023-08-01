f12bd3c6729b7055dee9373fdc2c812461e03c99
BogdanGlazkov <bglazkov@i.ua>
2023-08-01 18:46:14 +0300

    Add base components

---
+++ components/LearnSectionRecord.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LearnSectionRecord() {
+  return (
+    <div>
+      <h1>LearnSectionRecord</h1>
+    </div>
+  );
+}
---
+++ components/PricingSectionRecord.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PricingSectionRecord() {
+  return (
+    <div>
+      <h1>PricingSectionRecord</h1>
+    </div>
+  );
+}
