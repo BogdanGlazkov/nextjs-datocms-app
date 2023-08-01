f6e9cba680d589a3f2477585d0410a068530fe36
BogdanGlazkov <bglazkov@i.ua>
2023-08-01 18:24:14 +0300

    Setup graphql

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,44 @@
+import { gql, GraphQLClient } from "graphql-request";
+
 export default function Home() {
   return (
     <main className="w-full">
       <h1>Testy Test</h1>
     </main>
   );
+}
+
+const query = gql`
+  query {
+    course {
+      id
+      name
+      slug
+      courseDetail {
+        ... on CourseHeaderRecord {
+          __typename
+          bigTitle
+          smallTitle
+          buttonText
+          description
+          id
+        }
+      }
+    }
+  }
+`;
+
+export async function getStaticProps() {
+  const endpoint = "https://graphql.datocms.com/";
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      "content-type": "application/json",
+      authorization: "Bearer " + process.env.DATOCMS_API_KEY,
+    },
+  });
+  const course = await graphQLClient.request(query);
+  console.log(course);
+  return {
+    props: course,
+  };
 }
