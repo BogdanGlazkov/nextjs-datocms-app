b421ed05112637f8bfc304b3d1bff0c447bbb710
BogdanGlazkov <bglazkov@i.ua>
2023-08-02 19:03:27 +0300

    Add PricingSectionRecord styles

--- components/PricingSectionRecord.jsx
+++ components/PricingSectionRecord.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import PricingCard from "./PricingCard";
 
-export default function PricingSectionRecord() {
+export default function PricingSectionRecord({ details }) {
   return (
-    <div>
-      <h1>PricingSectionRecord</h1>
-    </div>
+    <section className="bg-purple-600 py-20 px-10 max-w-100">
+      <h2 className="text-center text-8xl font-bold text-white mb-20">
+        {details.title}
+      </h2>
+      <div className="flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:space-x-4 justify-center flex-wrap max-w-6xl mx-auto items-center">
+        {details.pricingCards.map((card, index) => (
+          <PricingCard details={card} key={index} />
+        ))}
+      </div>
+    </section>
   );
 }
